dd816ec98a63cac2807ff518a5e1595aa81b72b6
Solomon-Asefa <soloasefa6603@gmail.com>
2025-07-28 10:09:48 +0300

    Initial module structure for partner_import_wizard

---
+++ __manifest__.py
@@ -0,0 +1,11 @@
+{
+    'name': 'Partner Import Wizard',
+    'version': '1.0',
+    'summary': 'Wizard to import or update res.partner records from CSV/Excel',
+    'category': 'Tools',
+    'author': 'Solo Ak',
+    'depends': ['base', 'contacts'],
+    'data': [],
+    'installable': True,
+    'application': False,
+}
